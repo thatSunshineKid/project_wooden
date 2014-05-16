c90c5340382f1a992930776ba06575094fb1f62e
Adam Sunshine <adam@csuac.com>
2014-05-16 14:50:24 -0600

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectWooden::Application.config.secret_key_base = 'b1d030418ed916a38b7c2c33873b5a90bdd0bc07e0147073737c705e05860b1f55c1d4a2e0d261ef399ba81af1648a09ee1d42dbded70bd215cc5c0767634810'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectWooden::Application.config.session_store :cookie_store, key: '_project_wooden_session'
